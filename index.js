138db390d4241927fe4e03bc81f6909af6244057
Diana Jordan <dianacjordan@gmail.com>
2023-02-21 17:33:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+
+  let cost
+ if (distance > 2500){
+     cost = 'No can do.'
+ } else if (distance > 2000) {
+cost = "I will gladly take your thirty bucks."
+ } else if (distance > 400) {
+  cost = "That will be twenty bucks."
+   } else {
+    cost = 'This one is on me!'
+   }
+
+ return cost
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  let msg
+   msg = city ===  "NYC" ? "Ok, sounds good." : "No go." 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+   return msg
+ }
+
+function switchOnCharmFromTip(amount){
+  let resp
+  switch  (amount){
+    case "generous" : resp = "Thank you so much.";
+    break;
+    case "not as generous" : resp = "Thank you.";
+    break;
+    case "thanks for everything" : resp = "Bye."
+  }
+
+  return resp
 }
